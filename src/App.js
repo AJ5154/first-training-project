f41d45c46ea3ef4af06422a4d78ec05e8cf290a2
AJ5154 <100461996+AJ5154@users.noreply.github.com>
2023-07-22 15:40:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quod
+        earum possimus eaque voluptatem ipsam quasi architecto quae voluptates,
+        praesentium molestias voluptas exercitationem, ad soluta non magni nobis
+        ducimus accusamus reiciendis repellendus asperiores! Rem corrupti
+        aliquam pariatur, repudiandae quis dicta cum sequi, ea optio sint
+        laudantium quod hic error quam!
+      </p>
+      <img
+        src="https://staticimg.amarujala.com/assets/images/2023/07/21/750x506/ind-vs-wi-2023-live-score-ind-vs-wi-live-score-ind-vs-wi-test-live-score-ind-vs-wi-2nd-test-live_1689884749.jpeg?w=414"
+        alt="cricket"
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga maiores
+        obcaecati voluptate, culpa eius commodi quis distinctio rerum id
+        praesentium nulla quisquam aspernatur sit quas assumenda hic eaque
+        nesciunt. Quo quos, odit sint optio aperiam velit ipsa. Odio sequi,
+        dolorem perferendis ex ratione nesciunt qui est dolores aspernatur
+        corrupti deserunt accusamus blanditiis vel distinctio repellendus
+        veritatis ducimus laborum quod molestias dolore rerum harum sed et
+        officiis! Aperiam rerum deleniti porro voluptates sint veniam error
+        explicabo molestias ex dicta ipsum non odit amet nemo alias dolores
+        libero, consequuntur quibusdam soluta reprehenderit totam deserunt
+        labore sunt! Fugit nostrum voluptatibus quidem mollitia minus?
+      </p>
+    </div>
+  );
+}
+
+export default App;
